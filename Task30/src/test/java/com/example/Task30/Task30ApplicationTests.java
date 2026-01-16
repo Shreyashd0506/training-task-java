4a01160ae4580be3b5abd342ce809d9d4301717c
Shreyash Devtarase <160999035+Shreyashd0506@users.noreply.github.com>
2026-01-16 17:40:52 +0530

    Add files via upload

---
+++ Task30/src/test/java/com/example/Task30/Task30ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Task30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
